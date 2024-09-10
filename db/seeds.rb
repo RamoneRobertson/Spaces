a4f5a3f287aaeb45697e5b54936d453f1d52b91d
Ramone Robertson <ramonerobertson10@gmail.com>
2024-09-10 22:53:03 +0900

    Added seed file and ran migration for cafe model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "open-uri"
+
+puts "removing all cafes from the database..."
+Cafe.destroy_all
+
+puts "Getting cafes from json..."
+seed_url = 'https://gist.githubusercontent.com/yannklein/5d8f9acb1c22549a4ede848712ed651a/raw/3daec24bcd833f0dd3bcc8cee8616a731afd1f37/cafe.json'
+
+# Make an HTTP request to get the json data back
+json_cafes = URI.open(seed_url).read
+
+# Convert json data into a ruby object
+cafes = JSON.parse(json_cafes)
+
+# iterate over the array of hashes to create instances of cafes
+cafes.each do |cafe_hash|
+  puts "Creating #{cafe_hash["title"]...}"
+  Cafe.create!(
+    title: cafe_hash["title"],
+    address: cafe_hash["address"],
+    picture: cafe_hash["picture"],
+    criteria: cafe_hash["criteria"],
+    hours: cafe_hash["hours"]
+  )
+end
+
+puts "... created #{Cafe.count} cafes!"
